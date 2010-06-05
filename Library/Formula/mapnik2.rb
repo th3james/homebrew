98dac84bd338ca1c101524d56f29ebe68919195f
Dane Springmeyer <dane@dbsgeo.com>
2010-06-04 21:03:32 -0700

    tweaks to package dependencies and order as per learning from http://github.com/mxcl/homebrew/issues/issue/1451/

--- Library/Formula/mapnik2.rb
+++ Library/Formula/mapnik2.rb
@@ -8,17 +8,20 @@ class Mapnik2 <Formula
   depends_on 'scons'
   
   # core dependencies
+  depends_on 'libpng'
+  depends_on 'jpeg'
+  depends_on 'libtiff'
+  depends_on 'proj'
   depends_on 'icu4c'
   depends_on 'boost'
-  depends_on 'proj'
-  depends_on 'jpeg'
-  depends_on 'libpng'
-  depends_on 'libtiff'
   
-  # dependencies for plugins
-  depends_on 'gdal'
-  depends_on 'postgis'
-  depends_on 'sqlite'
+  # Mapnik will build plugins for these optional dependencies if they exist.
+  # if not it will skip. Seems safer to not enforce these, as users may want
+  # to build against Framework versions provided by http://www.kyngchaos.com/
+  # Todo - put in caveat...
+  #depends_on 'gdal'
+  #depends_on 'postgis'
+  #depends_on 'sqlite'
   
   def options
     [
@@ -27,9 +30,8 @@ class Mapnik2 <Formula
   end
 
   if ARGV.include? '--with-cairo'
-    depends_on 'cairo' if MACOS_VERSION == 10.5
-    depends_on 'cairomm'
     depends_on 'pkg-config'
+    depends_on 'cairomm'
   end   
   
   def install
@@ -41,6 +43,7 @@ class Mapnik2 <Formula
       "ICU_LIBS=#{icu.lib}",
       "ICU_INCLUDES=#{icu.include}",
       "INPUT_PLUGINS=shape,raster,gdal,postgis,ogr,sqlite,osm",
+      "PGSQL2SQLITE=True",
       "configure"
       ]
     
