a3ced1bca99724224f91957182052ad4bd1885e1
Christian Spanring <christian@spanring.eu>
2011-01-30 20:25:51 -0500

    update cmake in QGIS Formula

--- Library/Formula/qgis.rb
+++ Library/Formula/qgis.rb
@@ -7,14 +7,14 @@ class Qgis <Formula
 
   depends_on 'cmake' => :build
   depends_on 'postgis'
-  depends_on 'python' if ARGV.include? "--framework --universal"
+  depends_on 'python' # TODO: requires python "--framework --universal", check for "Python.framework"
   depends_on 'qt'
   depends_on 'gdal'
   depends_on 'geos'
   depends_on 'proj'
   depends_on 'sqlite'
   depends_on 'libspatialite' => :optional # causing problems, switched to internal lib
-  depends_on 'libiconv' => :optional
+  depends_on 'libiconv'
   depends_on 'gsl'
   depends_on 'pyqt'
   depends_on 'sip'
@@ -25,19 +25,16 @@ class Qgis <Formula
     geos = Formula.factory 'geos'
     gdal = Formula.factory 'gdal'
     python = Formula.factory 'python'
+    gsl = Formula.factory 'gsl'
     
-    system "cmake -DCMAKE_INSTALL_PREFIX=~/Applications -DCMAKE_BUILD_TYPE=Release -DCMAKE_BUILD_TYPE=MinSizeRel -DWITH_INTERNAL_SPATIALITE=TRUE -DGEOS_INCLUDE_DIR=#{geos.include} -DGEOS_LIBRARY=#{geos.lib}/libgeos_c.dylib -DGDAL_INCLUDE_DIR=#{gdal.include} -DGDAL_LIBRARY=#{gdal.lib}/libgdal.dylib -DPYTHON_EXECUTABLE=#{python.bin}/python -DPYTHON_LIBRARY=#{python.prefix}/Frameworks/Python.framework"
-    
+    system "cmake -DCMAKE_INSTALL_PREFIX=~/Applications -DCMAKE_BUILD_TYPE=Release -DCMAKE_BUILD_TYPE=MinSizeRel -DWITH_INTERNAL_SPATIALITE=TRUE -DGEOS_INCLUDE_DIR=#{geos.include} -DGEOS_LIBRARY=#{geos.lib}/libgeos_c.dylib -DGDAL_INCLUDE_DIR=#{gdal.include} -DGDAL_LIBRARY=#{gdal.lib}/libgdal.dylib -DGSL_INCLUDE_DIR=#{gdal.include} -DGSL_LIBRARIES=#{gsl.lib} -DPYTHON_EXECUTABLE=#{python.bin}/python"
     system "make install"
   end
   
-  def caveats
-    caveats = <<-EOS.undent
-      Quantum GIS was installed in
+  def caveats; <<-EOS.undent
+      Quantum GIS 1.6 was installed in
       ~/Applications
-
     EOS
-    return caveats
   end
   
 end
